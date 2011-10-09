e1d897ee6188f220199c7696bf382ae9fec80efd
Benjamin Gorman <gorman.benjamin@gmail.com>
2011-10-09 23:28:36 +0100

    Initital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gormify::Application.config.secret_token = '392de3c3df8900711eefb03a1c8adcd1d256482e3a234d99788e268962ad704bb3762a46e4f3a565c3880359c32b811948d9732c80dba6d0cf1afd3baada7660'
